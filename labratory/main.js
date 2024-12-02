6ff44303aa12dec5586e5a53ece552fb32df941a
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-01 20:21:58 -0500

    learning functions

--- labratory/main.js
+++ labratory/main.js
@@ -4,3 +4,19 @@ let thing = "did a thing!";
 function dostuff() {
     stuff.innerHTML = thing;
 }
+
+// function expression
+
+let sum = function(a, b) {
+    return a + b;
+};
+
+console.log(sum(4, 12));
+
+// and this is a function declaration 
+
+function sumOf2(a, b) {
+    return a + b;
+}
+
+console.log(sumOf2(42, 77));
