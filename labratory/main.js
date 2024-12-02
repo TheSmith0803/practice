43a6892f45904f3ea69078c77666ac3212a7ca13
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-01 19:45:54 -0500

    fixed selected element

--- labratory/main.js
+++ labratory/main.js
@@ -1,5 +1,5 @@
-let stuff = document.getElementsByClassName('stuff');
-let thing = "did a thing!"
+let stuff = document.querySelector('.stuff-child');
+let thing = "did a thing!";
 
 function dostuff() {
     stuff.innerHTML = thing;
