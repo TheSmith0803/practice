6994d01c4505ab5428435264f177019264ebf6a3
TheSmith0803 <ethan.m.smith0803@gmail.com>
2024-12-12 18:16:33 -0500

    concat method

--- labratory/main.js
+++ labratory/main.js
@@ -1,28 +1,6 @@
-let stuff = document.querySelector('.stuff-child');
-let thing = "did a thing!";
+const str1 = "Hi ";
+const str2 = "Mom!";
 
-function dostuff() {
-    stuff.innerHTML = thing;
-}
+const message = str1.concat(str2);
 
-// function expression
-
-let sum = function(a, b) {
-    return a + b;
-};
-
-console.log(sum(4, 12));
-
-// and this is a function declaration 
-
-function sumOf2(a, b) {
-    return a + b;
-}
-
-console.log(sumOf2(42, 77));
-
-// and this would be the way to use an arrow function
-
-let arrowSum = (a, b) => a + b;
-
-console.log(arrowSum(34, 22,));
+console.log(message);
