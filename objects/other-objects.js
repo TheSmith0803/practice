4ee797b3c78e5cd708af27e2d189da053d879a5d
TheSmith0803 <ethan.m.smith0803@gmail.com>
2025-01-13 21:28:58 -0500

    more object fun

--- objects/other-objects.js
+++ objects/other-objects.js
@@ -15,3 +15,82 @@ let person2 = person;
 person2.name = ["Billy", "Bob"];
 
 console.log(person.name + "\n" + person2.name);
+
+person.farewell = function () {
+    console.log("Bye");
+}
+
+person.farewell();
+
+
+
+const myDataName = "height";
+const myDataValue = "1.75m"
+person[myDataName] = myDataValue;
+
+console.log(person.height);
+
+console.log(person);
+
+
+
+const other = {
+    name: "Chris",
+    introduceSelf() {
+        console.log(`Hi! I'm ${this.name}.`);
+    },
+};
+
+const other2 = {
+    name: "Deepti",
+    introduceSelf() {
+        console.log
+    }
+}
+
+other.introduceSelf();
+
+//CONSTRUCTORS??
+
+//this way we can return an ojbect directly from a function
+//but it is a little bit more verbose
+
+function createPerson(name) {
+    const obj = {};
+    obj.name = name;
+    obj.introduceSelf = function() {
+        console.log(`Hi! I'm ${this.name}`);
+    };
+    return obj;
+}
+
+let me = "Ethan"
+
+const itsame = createPerson(me);
+
+itsame.introduceSelf();
+
+const frankie = createPerson("Frankie");
+frankie.introduceSelf();
+
+//using a constructor we can be a little more concise
+//constructors start with a capital letter by convention
+
+function Person(name) {
+    this.name = name;
+    this.introduceSelf = function () {
+        console.log(`Hi! I'm ${this.name}`);
+    };
+}
+
+//spaces because
+console.log("\n\n\n");
+
+//and then to call Person() as a constructor, we use the new keyword
+const salva = new Person("Salva");
+salva.introduceSelf();
+
+const fred = new Person("Fred");
+fred.introduceSelf();
+
+
